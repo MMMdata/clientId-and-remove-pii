4686622f9b171cd32ed93c60727b09c24dab10cb
MMMdata <eric.erlebacher@gmail.com>
2018-04-24 17:13:17 -0700

    Wrapped the whole thing in a script tag

--- clientId-and-remove-pii.js
+++ clientId-and-remove-pii.js
@@ -1,42 +1,44 @@
-function() {
-  return function(model) {
-  
-    // Specify the PII regular expressions
-    var piiRegex = [{
-      name: 'EMAIL',
-      regex: /.{4}@.{4}/g
-    }];
-
-    var globalSendTaskName = '_' + model.get('trackingId') + '_originalSendTask';
+<script>
+  function() {
+    return function(model) {
     
-    var originalSendTask = window[globalSendTaskName] = window[globalSendTaskName] || model.get('sendHitTask');
+      // Specify the PII regular expressions
+      var piiRegex = [{
+        name: 'EMAIL',
+        regex: /.{4}@.{4}/g
+      }];
 
-    var customDimensionIndex = 1;
-
-    model.set('dimension' + customDimensionIndex, model.get('clientId'));
-
-    var i, hitPayload, parts, val;
-    
-    model.set('sendHitTask', function(sendModel) {
-    
-      // Clear the payload of PII:
-      hitPayload = sendModel.get('hitPayload').split('&');
+      var globalSendTaskName = '_' + model.get('trackingId') + '_originalSendTask';
       
-      for (i = 0; i < hitPayload.length; i++) {
-        parts = hitPayload[i].split('=');
+      var originalSendTask = window[globalSendTaskName] = window[globalSendTaskName] || model.get('sendHitTask');
+
+      var customDimensionIndex = 1;
+
+      model.set('dimension' + customDimensionIndex, model.get('clientId'));
+
+      var i, hitPayload, parts, val;
+      
+      model.set('sendHitTask', function(sendModel) {
+      
+        // Clear the payload of PII:
+        hitPayload = sendModel.get('hitPayload').split('&');
         
-        // Double-decode, to account for web server encode + analytics.js encode
-        val = decodeURIComponent(decodeURIComponent(parts[1]));
-        
-        piiRegex.forEach(function(pii) {
-          val = val.replace(pii.regex, '[REDACTED ' + pii.name + ']');
-        });
-        
-        parts[1] = encodeURIComponent(val);
-        hitPayload[i] = parts.join('=');
-      }
-      sendModel.set('hitPayload', hitPayload.join('&'), true);
-      originalSendTask(sendModel);
-    });
+        for (i = 0; i < hitPayload.length; i++) {
+          parts = hitPayload[i].split('=');
+          
+          // Double-decode, to account for web server encode + analytics.js encode
+          val = decodeURIComponent(decodeURIComponent(parts[1]));
+          
+          piiRegex.forEach(function(pii) {
+            val = val.replace(pii.regex, '[REDACTED ' + pii.name + ']');
+          });
+          
+          parts[1] = encodeURIComponent(val);
+          hitPayload[i] = parts.join('=');
+        }
+        sendModel.set('hitPayload', hitPayload.join('&'), true);
+        originalSendTask(sendModel);
+      });
+    }
   }
-}
+</script>
